a4cb0d4760a7a4cf1ce943fdd9f29bf591f15c5a
Aleksa Trampa <alektramp@gmail.com>
2021-12-29 13:41:52 +0100

    Created Weapon Script with Raycast Logic when firing

--- Assets/_Project/Scripts/Weapon.cs
+++ Assets/_Project/Scripts/Weapon.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    RaycastHit hit;
 
     // Update is called once per frame
     void Update()
     {
+        HandleWeaponInput();
+    }
+
+    private void HandleWeaponInput()
+    {
+        if (!Input.GetKeyDown(KeyCode.Mouse0)) return;
         
+        Debug.Log("Weapon Fired");
+            
+        // Does the ray intersect any objects excluding the player layer
+        var rayHitTarget = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit, Mathf.Infinity);
+        if (rayHitTarget)
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * hit.distance, Color.yellow);
+            
+            return;
+        }
+        
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * 1000, Color.white);
+
     }
 }
