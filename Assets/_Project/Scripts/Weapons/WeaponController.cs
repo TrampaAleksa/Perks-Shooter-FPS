13cf227201fc9c27c3f00c4ff49e5921a1aeb204
Aleksa Trampa <alektramp@gmail.com>
2022-01-28 12:16:45 +0100

    Rate of fire added to weapons

--- Assets/_Project/Scripts/Weapons/WeaponController.cs
+++ Assets/_Project/Scripts/Weapons/WeaponController.cs
@@ -8,29 +8,37 @@ namespace _Project.Scripts.Weapons
         private IWeaponController _weaponController;
         public Weapon currentWeapon;
 
+        private TimedAction _timedAction;
+        private bool _canFireWeapon = true;
+
         private void Awake()
         {
             _weaponController = this;
+            _timedAction = gameObject.AddComponent<TimedAction>().DestroyOnFinish(false); // todo - consider wrapping the timed action
         }
-
         private void Update()
         {
             _weaponController.HandleWeaponControls();
         }
 
 
-        public void HandleWeaponControls()
+        public void HandleWeaponControls() //todo - extract interface for controls with rate of fire and without rate of fire
         {
-            if (!IsShotTriggered())
-                return;
-        
-            Debug.Log("Weapon Shot Triggered");
-            CurrentWeapon.Shoot();
-        }
+            if (IsShotTriggerPressed()){}
 
-        private static bool IsShotTriggered()
-        {
-            return Input.GetKeyDown(KeyCode.Mouse0);
+            if (IsShotTriggerHeld())
+            {
+                if (_canFireWeapon)
+                {
+                    Debug.Log("Weapon Shot Triggered");
+                    CurrentWeapon.Shoot();
+                    _canFireWeapon = false;
+                    StartWeaponCooldown();
+                }
+            }
+
+            if (IsShotTriggerReleased()){}
+
         }
 
         public Weapon CurrentWeapon
@@ -43,6 +51,15 @@ namespace _Project.Scripts.Weapons
                 currentWeapon.gameObject.SetActive(true);
             }
         }
+
+        private static bool IsShotTriggerPressed()
+            => Input.GetKeyDown(KeyCode.Mouse0);
+        private static bool IsShotTriggerHeld()
+            => Input.GetKey(KeyCode.Mouse0);
+        private static bool IsShotTriggerReleased()
+            => Input.GetKeyUp(KeyCode.Mouse0);
+        private void StartWeaponCooldown()
+            => _timedAction.StartTimedAction(() => _canFireWeapon = true, CurrentWeapon.rateOfFire);
     }
 
 }
